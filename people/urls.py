cc39ba27b49a2447dfdfc25186d1a99fcf845c2f
butuz0 <y.o.oryshchenko@gmail.com>
2023-12-08 23:56:35 +0200

    add link to post creation form

--- people/urls.py
+++ people/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from blog import views as blog_views
 
 app_name = 'people'
 
@@ -8,4 +9,5 @@ urlpatterns = [
     path('<username>', views.user_detail, name='user_detail'),
     path('activity/', views.user_activity, name='user_activity'),
     path('like/', views.like_user, name='like_user'),
+    path('<username>/new_post/', blog_views.create_post, name='create_post'),
 ]

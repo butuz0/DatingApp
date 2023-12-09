999f5ccceff6f065136bf7d6dced44ed80ffe1f6
butuz0 <y.o.oryshchenko@gmail.com>
2023-12-09 23:29:39 +0200

    display all tables on admin panel

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, Like, GroupOfInterests, Interest
 
 
 # Register your models here.
@@ -7,3 +7,18 @@ from .models import UserProfile
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'date_of_birth', 'gender', 'gender_preference', 'photo']
     raw_id_fields = ['user']
+
+
+@admin.register(Like)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user_from', 'user_to', 'created']
+
+
+@admin.register(GroupOfInterests)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(Interest)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['name', 'group']

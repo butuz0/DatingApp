794a2ea172679793eb1e1f689f2fed5b88c2c9b3
butuz0 <y.o.oryshchenko@gmail.com>
2023-12-19 16:54:03 +0200

    create filters for interests page

--- account/templatetags/account_extras.py
+++ account/templatetags/account_extras.py
@@ -1,4 +1,5 @@
 from django import template
+from account.models import Interest
 
 register = template.Library()
 
@@ -16,3 +17,15 @@ def add_attr(field, css):
             attrs[key] = val
 
     return field.as_widget(attrs=attrs)
+
+
+@register.filter
+def get_qs_values(queryset, key):
+    qs = queryset.all().values_list(key)
+    return [value[0] for value in qs]
+
+
+@register.filter
+def interest_to_str(value):
+    val = str(value).split('>')[-2][2:-7]
+    return str(val)

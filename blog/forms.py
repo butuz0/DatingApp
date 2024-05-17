d4f4882d867034c16f221196a2d7d0945361ad76
butuz0 <y.o.oryshchenko@gmail.com>
2024-05-17 13:56:25 +0300

    create form for adding comment to post

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,19 @@
+from django import forms
 from django.forms import ModelForm
-from .models import Post
+from .models import Post, Comment
 
 
 class CreatePostForm(ModelForm):
     class Meta:
         model = Post
         fields = ['text', 'image']
+
+
+class CreateCommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text']
+        widgets = {
+            'text': forms.Textarea(attrs={'placeholder': 'Add new comment',
+                                          'rows': '1'})
+        }

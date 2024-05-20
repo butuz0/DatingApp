2ae5f2daf0ad6a8b0c0b6e53ab5f4d6c7e695331
butuz0 <y.o.oryshchenko@gmail.com>
2024-05-20 12:01:03 +0300

    fix profile visits functions

--- statistic/redis_utils/redis_utils.py
+++ statistic/redis_utils/redis_utils.py
@@ -7,19 +7,13 @@ r = redis.Redis(host=settings.REDIS_HOST,
                 db=settings.REDIS_DB)
 
 
+# increment today`s users profile visits by one
 def increment_profile_visits(user_id):
-    # Отримання поточної дати
     today = date.today().strftime('%d-%m-%Y')
-    print(user_id)
-    print(today)
-    # Формування ключа для зберігання даних
-    # redis_key = f'user:{user_id}:profile_visits:{today}'
-    # Збільшення лічильника на 1
-    # r.incr(redis_key)
+    redis_key = f'user:{user_id}:profile_visits:{today}'
+    r.incr(redis_key)
 
 
 def get_profile_visits(user_id, day):
-    # Формування ключа для отримання даних
     redis_key = f'user:{user_id}:profile_visits:{day}'
-    # Отримання кількості відвідувань
     return int(r.get(redis_key) or 0)

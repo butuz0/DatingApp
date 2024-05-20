852a08d4dd98ec051741315c71a7a944e5bb73a9
butuz0 <y.o.oryshchenko@gmail.com>
2024-05-21 01:25:08 +0300

    create urls for stats

--- statistic/urls.py
+++ statistic/urls.py
@@ -6,5 +6,7 @@ app_name = 'statistic'
 urlpatterns = [
     path('', views.index, name='index'),
     path('profile_likes/', views.profile_likes, name='profile_likes'),
-    path('profile_likes_data/<int:days>/', views.get_profile_likes_data, name='profile_likes_data')
+    path('profile_likes_data/<int:days>/', views.get_profile_likes_data, name='profile_likes_data'),
+    path('relationship_types/', views.relationship_types_analysis, name='users_analysis'),
+    path('age_groups/', views.age_analysis, name='users_analysis2')
 ]
